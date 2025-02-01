5ef6e3db7c56bc7b71bc327458bc2bcad59fa896
sanskriti-ss <114874357+sanskriti-ss@users.noreply.github.com>
2025-02-01 10:39:43 -0800

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,12 @@
+# make sure you have streamlit. pip install streamlit
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import re
+import json
+import csv
+
+def main():
+    st.title("My First Streamlit App")
+    st.write("Hello, world! This is my first Streamlit application.")
